c5ed2f6538b4f8c65ab61343d3de7ddfb9807131
Johndiddles <johnadepelumi@gmail.com>
2025-05-02 20:34:20 +0100

    remove prisma related files

--- prisma/migrations/20250502061200_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Detpartment" (
-    "id" SERIAL NOT NULL,
-    "name" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Detpartment_pkey" PRIMARY KEY ("id")
-);
